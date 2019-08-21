3b786ec6bbe275ee18e0457429afc1b19993790c
razonyang <razonyang@gmail.com>
2019-08-21 16:12:30 +0800

    Convert non UserException or HttpException to ServerErrorHttpException in non-debug mode

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -3,12 +3,19 @@ namespace RazonYang\Yii2\JSend;
 
 use RazonYang\JSend\Exception;
 use RazonYang\JSend\Status;
+use Yii;
+use yii\base\UserException;
 use yii\web\ErrorHandler as BaseErrorHandler;
+use yii\web\HttpException;
 
 class ErrorHandler extends BaseErrorHandler
 {
     protected function convertExceptionToArray($exception)
     {
+        if (!YII_DEBUG && !$exception instanceof UserException && !$exception instanceof HttpException) {
+            $exception = new Exception(Yii::t('yii', 'An internal server error occurred.'), 500);
+        }
+
         $payload = [
             'status' => Status::ERROR,
             'message' => $exception->getMessage(),
